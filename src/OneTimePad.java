2e0ecab9ade62ce6c1c5ade6817399299e808269
tanyucel <tanyuecel@hotmail.com>
2016-11-14 13:51:36 +0100

    testing

--- src/OneTimePad.java
+++ src/OneTimePad.java
@@ -1,11 +1,160 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.NavigableMap;
 
 /**
  * Created by Tan on 10.03.2016.
  */
 public class OneTimePad {
     public static void main(String[] args) {
+        String[] stringsOneThree = new String[]{
+                "242C212E463D04061F123127563E02167801353C030C2622212E51097213382C1E1D3A57192F15250C21066F57311C33560A2B0F1C041B16204A39081921304514372234281C2C662A6C2E3F00152D0A394E3E11321D6F21002120462D04033C243970",
+                "23226828034E0F1F0D18743A1A3F0E1F3D037E6E2D06742B2139121A3B01282A520031124A7857625362133312161D3213073E0B0D0952032F093F470D3E741008223334220F2D2325382D2E52432C016B1A370A391A6F28146E390E07481925333F70"
+        };
 
+        BigInteger one = new BigInteger(stringsOneThree[0], 16);
+        BigInteger three = new BigInteger(stringsOneThree[1], 16);
+        BigInteger oneXORthree = one.xor(three);
+        System.out.println(oneXORthree.toString(16));
+
+        String[] stringsFiveSeven = new String[]{
+                "021E6825084E3D0C0C18313A12371A5E7806393A0D43242325240248340C3F6E210036190F6A063D0162302F1812103318493E0D4C0F1D18204A33094C2C74010F21222432192029256C292C060437182A1C360B741F2626522F6D100B0C0F254B",
+                "3528242303180F490118786B563E06522B10392A4B43761B2C2302483B106D2F5217361A1A26022B45310A350204003F190764423E100102270B7A0E1F6D3A0A12722C28610B3B232A6C272C52072A0C3E1D7C581D1D6F2E016E2309164807334B"
+        };
+
+
+        BigInteger two = new BigInteger(stringsFiveSeven[0], 16);
+        BigInteger four = new BigInteger(stringsFiveSeven[1], 16);
+        BigInteger fiveXORseven = two.xor(four);
+        System.out.println(fiveXORseven.toString(16));
+
+        String word = "68656c6c6f";
+        String longWord = "";
+        String[] permutes = new String[word.length()];
+        String[] results = new String[permutes.length];
+        String zeros = "0";
+
+
+        for (int i = 0; i < stringsOneThree[0].length() / word.length() + 1; i++) {
+            longWord += word;
+        }
+        longWord = longWord.substring(0, stringsOneThree[0].length() - 1);
+
+        for (int i = 0; i < word.length(); i++) {
+            permutes[i] = longWord.substring(i) + word;
+            permutes[i] = permutes[i].substring(0, stringsOneThree[0].length() - 1);
+            System.out.println(permutes[i]);
+        }
+        System.out.println();
+
+        /*
+        for (int i = 0; i < permutes.length; i++) {
+            results[i] = new BigInteger(permutes[i], 16).xor(oneXORthree).toString(16);
+            System.out.println();
+            System.out.println(results[i]);
+        }
+*/
+
+        for (int i = 0; i < permutes.length; i++) {
+            results[i] = new BigInteger(permutes[i], 16).xor(fiveXORseven).toString(16);
+            System.out.println();
+            System.out.println(results[i]);
+        }
+
+        System.out.println();
+        System.out.println();
+        String helper = "73706f6e6765626f62\n" +
+                "7061747269636b\n" +
+                "6b7261626279\n" +
+                "627572676572\n" +
+                "7061747479\n" +
+                "6d72\n" +
+                "6b72616273\n" +
+                "7065726c61\n" +
+                "737175696477617264\n" +
+                "62696b696e69\n" +
+                "626f74746f6d\n" +
+                "6c61727279\n" +
+                "6c6f6273746572\n" +
+                "66697368\n" +
+                "70696e656170706c65";
+
+        String[] words = helper.split("\\n");
+        ArrayList<ArrayList<String>> wordsPermutes = new ArrayList<>();
+        String buffer = "";
+        for (int i = 0; i < words.length; i++) {
+            wordsPermutes.add(i, new ArrayList<>());
+
+            for (int j = 0; j < stringsFiveSeven[0].length() / words[i].length() + 1; j++) {
+                buffer += words[i];
+            }
+            buffer = buffer.substring(0, stringsFiveSeven[0].length());
+
+            for (int j = 0; j < words[i].length(); j++) {
+                wordsPermutes.get(i).add(buffer.substring(j) + words[i]);
+                wordsPermutes.get(i).set(j, wordsPermutes.get(i).get(j).substring(0, stringsFiveSeven[0].length() - 1));
+            }
+
+            buffer = "";
+        }
+
+
+        ArrayList<String> manyResults = new ArrayList<>();
+
+        for (int i = 0; i < wordsPermutes.size(); i++) {
+
+            for (int j = 0; j < wordsPermutes.get(i).size(); j++) {
+                manyResults.add(new BigInteger(wordsPermutes.get(i).get(j), 16).xor(fiveXORseven).toString(16));
+                System.out.println();
+                System.out.println(words[i]);
+                System.out.println(new BigInteger(wordsPermutes.get(i).get(j), 16).xor(fiveXORseven).toString(16));
+                System.out.println();
+            }
+        }
+
+
+        //System.out.println(longWord);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
         String[] strings = new String[]{"242C212E463D04061F123127563E02167801353C030C2622212E51097213382C1E1D3A57192F15250C21066F57311C33560A2B0F1C041B16204A39081921304514372234281C2C662A6C2E3F00152D0A394E3E11321D6F21002120462D04033C243970"
                 , "021E6825084E3D0C0C18313A12371A5E7806393A0D43242325240248340C3F6E210036190F6A063D0162302F1812103318493E0D4C0F1D18204A33094C2C74010F21222432192029256C292C060437182A1C360B741F2626522F6D100B0C0F254B"
                 , "23226828034E0F1F0D18743A1A3F0E1F3D037E6E2D06742B2139121A3B01282A520031124A7857625362133312161D3213073E0B0D0952032F093F470D3E741008223334220F2D2325382D2E52432C016B1A370A391A6F28146E390E07481925333F70"
@@ -16,6 +165,7 @@ public class OneTimePad {
                 , "32392023050F0605115A743E1E330D522F1470220A0C3F6F253E511C3A066D3C17072C1B1E394B730C36433212001925561D22071F0052062B183F470228370015212023384A3D2E22222F395E4131072E1D3758230C3D2252382412030460"
                 , "3F286823154E0B0A0B03272C12760C1478073921090220262A2D511C3A066D0B01043018042B00364503003557041A32560F2B01091652103A4A36020D3E204555626128240B3B356B25266A18002C03654E01083108242E1C296D090C62"
                 , "3823646A0E0B4A0818063128042543113014353C0316386F252415482006212F0A113D594A0202730D23106116131B3F120C2E42180D1751280B2E024C22324500372D3D2E1D693123253B3E1E0427032419370A742A27221E3D280768"
+                , "3A242623151A0F1B4456262C10331100311F376E110C741B2C2F030D21026D03130D7916196A06734723433218170076190F6A260D1706196E3C3B03093F740C08723539244A1C2822382D2E522A2C012C0A3D1576456F301A213E0368"
         };
 
         String[][] xoredStrings = new String[strings.length][strings.length];
@@ -27,7 +177,7 @@ public class OneTimePad {
         String[][] stringResult = new String[strings.length][strings.length];
 
         int[][] xoredInts = new int[strings.length][strings.length];
-        String the = "The ";
+        String the = "that" ;
         the = toHex(the);
 
         for (int i = 0; i < the.length(); i++) {
@@ -93,7 +243,7 @@ public class OneTimePad {
         }
 
 */
-
+/*
         for (int i = 0; i < xored.length; i++) {
             for (int j = 0; j < xored[i].length; j++) {
 
@@ -114,13 +264,13 @@ public class OneTimePad {
                     the += "0";
                     //System.out.println(helper[i]);
                 }
-                checker(hexToString(helper), f);
+                checker(hexToString(helper), f - 1);
 
             }
 
         }
 
-
+*/
 
 
         /*
@@ -172,7 +322,8 @@ public class OneTimePad {
 */
 
     }
-
+}
+/*
     public static String[] hexToString(String[] arr) {
 
 
@@ -209,6 +360,7 @@ public class OneTimePad {
     }
 
     public static String toHex(String arg) {
-        return String.format("%040x", new BigInteger(1, arg.getBytes(/*YOUR_CHARSET?*/)));
+        return String.format("%040x", new BigInteger(1, arg.getBytes(//*YOUR_CHARSET?* /)));
     }
 }
+*/
